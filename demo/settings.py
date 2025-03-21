ecb85aad78a76f25fee15c9006f3d737ab4bea21
Koded0214h <coder0214h@gmail.com>
2025-03-21 15:42:06 +0100

    added templates

--- demo/settings.py
+++ demo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['demo-o4t0.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'demo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
